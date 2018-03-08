7a044d2bc0a8a75521c6c1052ed38ef7e0b45330
Eli Kozinets <elikozinetz@gmail.com>
2018-03-08 18:26:31 +0200

    One sided chat adding to database complete

--- app/src/main/java/il/co/appschool/firebasechatapp/ChatActivity.java
+++ app/src/main/java/il/co/appschool/firebasechatapp/ChatActivity.java
@@ -2,15 +2,19 @@ package il.co.appschool.firebasechatapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
@@ -19,6 +23,19 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 //        mAuth = FirebaseAuth.getInstance();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.input);
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+                input.setText("");
+            }
+        });
     }
 
     @Override
